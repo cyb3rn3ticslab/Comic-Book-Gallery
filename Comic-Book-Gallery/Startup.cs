24d870338c2a9c5f6545de5dc061cae4b5069a85
Md Saney Alam <codecamper@outlook.com>
2019-01-17 15:40:38 -0500

    List view to Detail View connection

--- Comic-Book-Gallery/Startup.cs
+++ Comic-Book-Gallery/Startup.cs
@@ -56,7 +56,7 @@ namespace ComicBookGallery
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=ComicBook}/{action=Index}/{id?}");
             });
         }
     }
